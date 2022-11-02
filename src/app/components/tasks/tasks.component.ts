8856b0e2186c7f51d0eb2e9063d6860de193886f
Eric Moran <emoran0403@gmail.com>
2022-11-02 10:46:03 -0400

    imported tasks from mock file into tasks parent component

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { Task } from '../../Task'; // Task interface
+import { TASKS } from '../../mock-tasks'; // mock tasks from file, will be replaced with tasks from server
 
 @Component({
   selector: 'app-tasks',
   templateUrl: './tasks.component.html',
-  styleUrls: ['./tasks.component.css']
+  styleUrls: ['./tasks.component.css'],
 })
 export class TasksComponent implements OnInit {
+  // for now, define tasks, and set that to the tasks from the mock file
+  tasks: Task[] = TASKS;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
